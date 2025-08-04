57431864a5e13c03c440db73311cfc1208f38088
Osei Rick <rickoseiampah@gmail.com>
2025-08-04 10:00:34 +0000

    cleared fonts and logos export and folder

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,14 +1,3 @@
-// Main assets exports
-// This file exports all assets from different categories
-
 export * from "./images";
 export * from "./icons";
-export * from "./logos";
-export * from "./fonts";
 export * from "./data";
-
-// You can also export specific assets directly here
-// Example:
-// export { heroImage, logo } from './images';
-// export { chevronDown, menu } from './icons';
-// export { mainLogo } from './logos';
